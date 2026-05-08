40ce937038efe7a8f3b32d4a3768d36f91007d1e
Vargoshi <marcinflisak1998@gmail.com>
2026-05-08 23:06:31 +0200

    use argument parser in decompressor tool

--- tools/decompressor/backwardLZ77.cpp
+++ tools/decompressor/backwardLZ77.cpp
@@ -1,5 +1,11 @@
+
+#include "../../lib/decompressor/backwardLZ77/backwardLZ77.h"
+#include "../../lib/argumentParser/ArgumentParser.h"
 #include "../../lib/filesystem/readFile/readFile.h"
 #include "../../lib/filesystem/writeFile/writeFile.h"
 #include <iostream>
 
-int main() { return 0; }
+int main(int argc, char **argv) {
+  openfranko::lib::argumentParser::ArgumentParser parser(argc, argv);
+  return 0;
+}
