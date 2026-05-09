91708be094c0cf57119c401e8d25f650e1c509d0
Vargoshi <marcinflisak1998@gmail.com>
2026-05-09 11:29:08 +0200

    add decompressor tool impl

--- tools/decompressor/backwardLZ77.cpp
+++ tools/decompressor/backwardLZ77.cpp
@@ -5,7 +5,30 @@
 #include "../../lib/filesystem/writeFile/writeFile.h"
 #include <iostream>
 
+using namespace openfranko::lib;
+
 int main(int argc, char **argv) {
-  openfranko::lib::argumentParser::ArgumentParser parser(argc, argv);
+  argumentParser::ArgumentParser parser(argc, argv);
+
+  if (parser.cmdOptionExists("-i")) {
+
+    std::string inputFilePath = parser.getCmdOption("-i").value();
+
+    std::string outputFilePath = inputFilePath + ".dec";
+
+    if (parser.cmdOptionExists("-o")) {
+      outputFilePath = parser.getCmdOption("-o").value();
+    }
+
+    std::vector<uint8_t> compressedData =
+        filesystem::readFile::readFile(inputFilePath);
+    std::vector<uint8_t> decompressedData =
+        decompressor::backwardLZ77::decompress(compressedData);
+    filesystem::writeFile::writeFile(outputFilePath, decompressedData);
+  } else {
+    std::cerr << "Usage: " << argv[0] << " -i <input_file> -o <output_file>"
+              << std::endl;
+  }
+
   return 0;
 }
