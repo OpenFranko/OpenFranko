a998f3386d877bec39d153dbeb34baf1c0400ec0
Vargoshi <marcinflisak1998@gmail.com>
2026-05-10 19:28:13 +0200

    add more functions to helpers

--- lib/decompressor/helpers/helpers.cpp
+++ lib/decompressor/helpers/helpers.cpp
@@ -9,4 +9,13 @@ uint32_t readUint32BigEndian(const std::vector<uint8_t> &data, size_t pos) {
          (static_cast<uint32_t>(data[pos + 3]) << 0);
 }
 
+uint16_t readUint16BigEndian(const std::vector<uint8_t> &data, size_t pos) {
+  return (static_cast<uint16_t>(data[pos + 0]) << 8) |
+         (static_cast<uint16_t>(data[pos + 1]) << 0);
+}
+
+int16_t readInt16BigEndian(const std::vector<uint8_t> &data, size_t pos) {
+  return static_cast<int16_t>(readUint16BigEndian(data, pos));
+}
+
 } // namespace openfranko::lib::decompressor::helpers
--- lib/decompressor/helpers/helpers.h
+++ lib/decompressor/helpers/helpers.h
@@ -8,6 +8,10 @@ namespace helpers {
 
 uint32_t readUint32BigEndian(const std::vector<uint8_t> &data, size_t pos);
 
+uint16_t readUint16BigEndian(const std::vector<uint8_t> &data, size_t pos);
+
+int16_t readInt16BigEndian(const std::vector<uint8_t> &data, size_t pos);
+
 } // namespace helpers
 } // namespace decompressor
 } // namespace lib
